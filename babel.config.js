0700c53096fd3b7cff7d19e9e77104db5db4ff09
steve zhang <871506263@qq.com>
2021-11-14 20:07:00 +0800

    提交代码于: 2021-11-14 20:07:00

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
-  plugins: [
-    '@babel/plugin-transform-modules-commonjs'
-  ],
-}
+  // presets: ['@babel/preset-env'], //不确定有没有用
+  plugins: ['@babel/plugin-transform-modules-commonjs'],
+};
