55a2cb22defefbb9fcc8d149033922aa36142dbc
steve zhang <871506263@qq.com>
2021-11-18 19:39:25 +0800

    提交代码于: 2021-11-18 19:39:25

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // require('./lesson/02');
 // require('./lesson/03');
 // require('./lesson/04');
-require('./lesson/05');
+// require('./lesson/05');
+require('./lesson/06');
---
+++ src/lesson/06.ts
@@ -0,0 +1,44 @@
+// 数字枚举
+// enum Direction {
+//   Up = 100,
+//   Down,
+//   Left,
+//   Right,
+// }
+// console.log(`Direction:`, Direction);
+
+// console.log(`Direction[100]:`, Direction[100]);
+// console.log(`Direction[101]:`, Direction[101]);
+// console.log(`Direction[102]:`, Direction[102]);
+// console.log(`Direction[103]:`, Direction[103]);
+// console.log(`Direction['Up']:`, Direction['Up']);
+// console.log(`Direction['Down']:`, Direction['Down']);
+// console.log(`Direction['Left']:`, Direction['Left']);
+// console.log(`Direction['Right']:`, Direction['Right']);
+
+// 字符串枚举
+// enum Direction1 {
+//   Up = 'u',
+//   Down = 'd',
+//   Left = 'l',
+//   Right = 'r',
+// }
+// console.log(`Direction1:`, Direction1);
+// console.log('----------------------------------');
+// console.log(`Direction1['Up']:`, Direction1['Up']);
+// console.log(`Direction1['Down']:`, Direction1['Down']);
+// console.log(`Direction1['Left']:`, Direction1['Left']);
+// console.log(`Direction1['Right']:`, Direction1['Right']);
+// console.log('----------------------------------');
+
+// 异构枚举
+// enum Egnum {
+//   NO = 0,
+//   YES = 'yes',
+// }
+
+// console.log(`Egnum`, Egnum);
+// console.log('----------------------------------');
+// console.log(`Egnum[0]:`, Egnum[0])
+// console.log(`Egnum['NO']:`, Egnum['NO'])
+// console.log(`Egnum['YES']:`, Egnum['YES'])
