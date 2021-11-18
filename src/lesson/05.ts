378174703274d86484b912edcfbe8a4c737f07aa
steve zhang <871506263@qq.com>
2021-11-18 19:25:42 +0800

    提交代码于: 2021-11-18 19:25:42

--- src/lesson/05.ts
+++ src/lesson/05.ts
@@ -34,14 +34,40 @@
 // console.log(`strArr:`, strArr);
 
 // 元组（Tuple）
-let x: [string, number];
-x = ['1', 2];
-console.log(`x 000:`, x);
-x.push('越界');
-console.log(`x 001:`, x);
-console.log('--------------------');
-console.log(`x.length:`, x.length);
-console.log(`x[0]:`, x[0]);
-console.log(`x[1]:`, x[1]);
-// ts允许向元组中使用数组的push方法插入新元素(但不允许访问)
-// console.log(`x[2]:`, x[2]);
+// let x: [string, number];
+// x = ['1', 2];
+// console.log(`x 000:`, x);
+// x.push('越界');
+// console.log(`x 001:`, x);
+// console.log('--------------------');
+// console.log(`x.length:`, x.length);
+// console.log(`x[0]:`, x[0]);
+// console.log(`x[1]:`, x[1]);
+// // ts允许向元组中使用数组的push方法插入新元素(但不允许访问)
+// // console.log(`x[2]:`, x[2]);
+
+// Object
+
+// 这是下一节会提到的枚举类型
+enum Direction {
+  Center = 1,
+}
+console.log(`typeof Direction:`, typeof Direction)
+let obj: object = {};
+console.log(`typeof obj:`, typeof obj)
+let arr: number[] = [1, 2, 3];
+console.log(`typeof arr:`, typeof arr)
+let tup: [number, string] = [88, 'hello'];
+console.log(`typeof tup:`, typeof tup);
+
+let value: object;
+value = obj; // 普通对象
+console.log(`value 000:`, value);
+value = Direction; // 枚举
+console.log(`value 001:`, value);
+value = arr; // 数组
+console.log(`value 002:`, value);
+value = tup;
+console.log(`value 003:`, value);
+
+// 普通对象、枚举、数组、元组通通都是 object 类型;
