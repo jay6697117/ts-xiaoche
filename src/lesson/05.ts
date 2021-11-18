5ff2c21f3925c561c4a5079d3695a48c231c1e24
steve zhang <871506263@qq.com>
2021-11-18 16:33:56 +0800

    提交代码于: 2021-11-18 16:33:56

--- src/lesson/05.ts
+++ src/lesson/05.ts
@@ -3,7 +3,6 @@
 // notsure = 'may be is a string';
 // console.log(`notsure:`, notsure);
 
-
 // unknown
 // function dateToString(param: unknown): string {
 //   if (param instanceof Date) {
@@ -27,3 +26,22 @@
 // console.log(`empty:`, empty)
 
 // error('我是错误');//报错
+
+// 数组
+// const numArr: Array<number> = [1, 2, 3];
+// console.log(`numArr:`, numArr);
+// const strArr: string[] = ['4', '5', '6'];
+// console.log(`strArr:`, strArr);
+
+// 元组（Tuple）
+let x: [string, number];
+x = ['1', 2];
+console.log(`x 000:`, x);
+x.push('越界');
+console.log(`x 001:`, x);
+console.log('--------------------');
+console.log(`x.length:`, x.length);
+console.log(`x[0]:`, x[0]);
+console.log(`x[1]:`, x[1]);
+// ts允许向元组中使用数组的push方法插入新元素(但不允许访问)
+// console.log(`x[2]:`, x[2]);
