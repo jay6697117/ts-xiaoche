fde40995fbc7b73ae3f18210b74ac009ef4de614
steve zhang <871506263@qq.com>
2021-12-09 22:47:45 +0800

    提交代码于: 2021-12-09 22:47:45

--- src/lesson/08.ts
+++ src/lesson/08.ts
@@ -66,7 +66,28 @@ const raceCar = new RaceCar();
 */
 
 // 4.0 class 可以作为接口
-// export default class Carousel extends React.Component<Props, State> {}
+// class Carousel extends React.Component<Props, State> {}
+
+
+/*
+// props的类型
+class Props {
+  public children: Array<React.ReactElement<any>> | React.ReactElement<any> | never[] = []
+  public speed: number = 500
+  public height: number = 160
+  public animation: string = 'easeInOutQuad'
+  public isAuto: boolean = true
+  public autoPlayInterval: number = 4500
+  public afterChange: () => {}
+  public beforeChange: () => {}
+  public selesctedColor: string
+  public showDots: boolean = true
+}
+*/
+
+
+// Props 的实例就是 defaultProps 的初始值
+// public static defaultProps = new Props()
 
 // 默认导出模块
 export {};
